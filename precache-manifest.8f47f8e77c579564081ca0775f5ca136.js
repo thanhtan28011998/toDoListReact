e59186a7ac1f4fb626131f54e3278592f1f1ea6e
thanhtan28011998 <thanhtan28011998@gmail.com>
2020-02-25 21:24:12 +0700

    Updates

---
+++ precache-manifest.8f47f8e77c579564081ca0775f5ca136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca448ab5e997ff30e6e1d7d5f8c6d302",
+    "url": "/toDoListReact/index.html"
+  },
+  {
+    "revision": "547afd27cdd27442f4e8",
+    "url": "/toDoListReact/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b22687602d43f8db3422",
+    "url": "/toDoListReact/static/js/2.2cd5693e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/toDoListReact/static/js/2.2cd5693e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547afd27cdd27442f4e8",
+    "url": "/toDoListReact/static/js/main.702ca0fb.chunk.js"
+  },
+  {
+    "revision": "9181df738fd013e34ad0",
+    "url": "/toDoListReact/static/js/runtime-main.c4f2c85f.js"
+  }
+]);
